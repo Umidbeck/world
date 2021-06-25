9b5ed749dd50114e355e2b04f60f6ce6e7f18786
Ars <umidx27@gmail.com>
2021-06-26 01:21:07 +0500

    Tamom

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name="home"),
+    path("create/", create, name="posts_create"),
+    path("delete/<int:id>", delete, name= "posts_delete"),
+    path("update/<int:id>", update, name= "posts_update"),
     path('about', AboutView.as_view(), name="about"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import *
+from django.shortcuts import redirect
 
 
 # class HomeView(ListView):
@@ -18,3 +19,37 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, "home.html", context)
+
+
+
+
+def create(request):
+    if request.method == "POST":
+        p= Post(title=request.POST['title'], text=request.POST['text'])
+        p.save()
+        return redirect("home")
+
+    return render(request, "create.html")
+
+
+def update(request, id):
+    try:
+        p = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        return redirect("home")
+
+    if request.method == "POST":
+        p.title =request.POST['title']
+        p.text = request.POST['text']
+        p.save()
+
+        return redirect("home")
+
+    return render(request, "create.html", {
+        "p": p
+    })
+
+
+def delete(request, id):
+    Post.objects.filter(id=id).delete()
+    return redirect("home")
