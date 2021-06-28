d2a45db6ae8100a98bbeb6521114f1c7edaf691c
Ars <umidx27@gmail.com>
2021-06-28 20:17:43 +0500

    ...1...

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
 class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=200, null=True)
-    author = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-        null=True
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     text = models.TextField(max_length=100)
+    image = models.ImageField(upload_to = 'images/', null = True)
+    create_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    update_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
